637bb382b35ab5b322e4791a42608ef9f3588164
Erik Konrad <erik.konrad@zebra.de>
2021-06-24 13:30:54 +0200

    adds flex object support for synchronizing directus collections with flex objects

--- directus.php
+++ directus.php
@@ -3,9 +3,16 @@ namespace Grav\Plugin;
 
 use Composer\Autoload\ClassLoader;
 use Grav\Common\Cache;
+use Grav\Common\Grav;
 use Grav\Common\Plugin;
+use Grav\Framework\Flex\Flex;
+use Grav\Framework\Flex\FlexObject;
+use Grav\Framework\Flex\Interfaces\FlexCollectionInterface;
+use Grav\Framework\Flex\Interfaces\FlexDirectoryInterface;
 use Grav\Plugin\Directus\Directus;
 use Grav\Plugin\Directus\Utility\DirectusUtility;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use RocketTheme\Toolbox\Event\Event;
 
 /**
@@ -14,6 +21,26 @@ use RocketTheme\Toolbox\Event\Event;
  */
 class DirectusPlugin extends Plugin
 {
+
+    /**
+     * @var Flex
+     */
+    protected $flex;
+
+    /**
+     * @var FlexCollectionInterface
+     */
+    protected $collection;
+
+    /**
+     * @var FlexDirectoryInterface
+     */
+    protected $directory;
+
+    /**
+     * @var DirectusUtility
+     */
+    protected $directusUtil;
 
     /**
      * @return array
@@ -72,6 +99,18 @@ class DirectusPlugin extends Plugin
      */
     public function onPageInitialized()
     {
+
+        /** @var Flex $flex */
+        $this->flex = Grav::instance()->get('flex');
+
+        $this->directusUtil = new DirectusUtility(
+            $this->config()['directus']['directusAPIUrl'],
+            $this->grav,
+            '',
+            '',
+            $this->config()['directus']['token'],
+            $this->config()['disableCors']
+        );
 
         $this->processWebHooks($this->grav['uri']->route());
     }
@@ -224,12 +263,227 @@ class DirectusPlugin extends Plugin
                 try {
                     $this->refreshSingleDataFiles();
                 } catch (\Exception $e) {
-                    $this->log($e);
+                    $this->writeLog($e);
                 }
 
                 break;
+            case '/' . $this->config()['directus']['hookPrefix'] . '/sync-flexobject':
+                $this->processFlexObject();
+                break;
+            case '/' . $this->config()['directus']['hookPrefix'] . '/sync-flexobjects':
+                $this->processFlexObjects();
+                break;
         }
         return true;
+    }
+
+    /**
+     * @throws \JsonException
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function processFlexObject() {
+        $requestBody = json_decode(file_get_contents('php://input'), true);
+
+        if(isset($_REQUEST['debug'])) {
+            $this->writeLog($this->buildLogEntry(json_encode( $requestBody, JSON_THROW_ON_ERROR), 'request from webhook'));
+        }
+
+        $statusCode = 0;
+
+        if(isset($requestBody['collection'])) {
+
+            /** @var FlexCollectionInterface $collection */
+            $this->collection = $this->flex->getCollection($requestBody['collection']);
+
+            /** @var FlexDirectoryInterface $directory */
+            $this->directory = $this->flex->getDirectory($requestBody['collection']);
+
+            $depth = 2;
+
+            foreach($this->config()['directus']['synchronizeTables'] as $collection => $config) {
+                if($collection === $requestBody['collection']) {
+                    $depth = $config['depth'];
+                }
+            }
+            try {
+                switch ($requestBody['action']) {
+                    case "create":
+                        $statusCode = $this->createFlexObject($requestBody['collection'], $requestBody['item'], $depth);
+                        break;
+                    case "update":
+                        $statusCode = $this->updateFlexObject($requestBody['collection'], $requestBody['item'], $depth);
+                        break;
+                    case "delete":
+                        $statusCode = $this->deleteFlexObject($requestBody['collection'], $requestBody['item']);
+                        break;
+                }
+            } catch(\Exception $e) {
+                $this->writeLog($this->buildLogEntry($e, 'something went wrong'));
+            }
+        }
+
+        if($statusCode === 200) {
+            echo json_encode([
+                'status' => '200',
+                'message' => 'all done'
+            ], JSON_THROW_ON_ERROR);
+            Cache::clearCache();
+            exit(200);
+        }
+
+        echo json_encode([
+            'status' => $statusCode,
+            'message' => 'something went wrong'
+        ], JSON_THROW_ON_ERROR);
+        if(isset($_REQUEST['debug'])) {
+            $this->writeLog($this->buildLogEntry($statusCode, 'something went wrong'));
+        }
+        exit($statusCode);
+    }
+
+    /**
+     * @throws \JsonException
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function processFlexObjects() {
+        $this->delTree('user/data/flex-objects');
+
+        $collectionArray = $this->config()['directus']['synchronizeTables'];
+
+        foreach ($collectionArray as $collection => $config){
+
+
+            /** @var FlexCollectionInterface $collection */
+            $this->collection = $this->flex->getCollection($collection);
+
+            /** @var FlexDirectoryInterface $directory */
+            $this->directory = $this->flex->getDirectory($collection);
+
+            $response = $this->requestItem($collection, 0, ($config['depth'] ?? 2));
+
+            foreach ($response->toArray()['data'] as $item){
+                $object = $this->collection->get($item['id']);
+
+                if ($object) {
+                    $object->update($item);
+                    $object->save();
+                } else {
+                    $objectInstance = new FlexObject($item, $item['id'], $this->directory);
+                    $object = $objectInstance->create($item['id']);
+                    $this->collection->add($object);
+                }
+            }
+        }
+        echo json_encode([
+            'status' => 200,
+            'message' => 'all done'
+        ], JSON_THROW_ON_ERROR);
+        Cache::clearCache();
+        exit(200);
+    }
+
+    /**
+     * @param $collection
+     * @param $id
+     * @param int $depth
+     * @return int
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function createFlexObject($collection, $id, int $depth = 2) {
+        $response = $this->requestItem($collection, $id, $depth);
+
+        if(isset($_REQUEST['debug'])) {
+            $this->writeLog($this->buildLogEntry(json_encode($response->toArray(), JSON_THROW_ON_ERROR), 'create flex object - request to directus | data = response from directus'));
+        }
+
+        if($response->getStatusCode() === 200) {
+            $data = $response->toArray()['data'];
+            $objectInstance = new FlexObject($data, $data['id'], $this->directory);
+            $object = $objectInstance->create($data['id']);
+            $this->collection->add($object);
+        }
+        return $response->getStatusCode();
+    }
+
+    /**
+     * @param $collection
+     * @param $ids
+     * @param int $depth
+     * @return int
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function updateFlexObject($collection, $ids, int $depth = 2) {
+        foreach ($ids as $id) {
+
+            try {
+                $response = $this->requestItem($collection, $id, $depth);
+
+                if(isset($_REQUEST['debug'])) {
+                    $this->writeLog($this->buildLogEntry(json_encode($response->toArray(), JSON_THROW_ON_ERROR), 'update flex object - request to directus | data = response from directus'));
+                }
+                if($response->getStatusCode() === 200) {
+                    $object = $this->collection->get($id);
+
+                    if ($object) {
+                        $object->update($response->toArray()['data']);
+                        $object->save();
+                    } else {
+                        $this->createFlexObject($collection, $id);
+                    }
+                }
+            } catch(\Exception $e) {
+                if(isset($_REQUEST['debug'])) {
+                    $this->writeLog($this->buildLogEntry(json_encode($e, JSON_THROW_ON_ERROR), 'something went wrong'));
+                }
+            }
+
+        }
+        return 200;
+    }
+
+    /**
+     * @param $collection
+     * @param $ids
+     * @return int
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function deleteFlexObject($collection, $ids) {
+        foreach ($ids as $id) {
+            $object = $this->collection->get($id);
+            if($object) {
+                $object->delete();
+            }
+        }
+        return 200;
+    }
+
+    /**
+     * @param $collection
+     * @param $id
+     * @param int $depth
+     * @return \Symfony\Contracts\HttpClient\ResponseInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function requestItem($collection, $id = 0, $depth = 2, $filters = []) {
+
+        $requestUrl = $this->directusUtil->generateRequestUrl($collection, $id, $depth, $filters);
+        return $this->directusUtil->get($requestUrl);
     }
 
     /**
@@ -269,9 +523,41 @@ class DirectusPlugin extends Plugin
         exit;
     }
 
-    protected function log($data) {
-        $fp = fopen('logs/directus.log', 'w');
-        fwrite($fp, $data);
-        fclose($fp);
+    /**
+     * @param $dir
+     */
+    private function delTree($dir){
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($files as $fileinfo) {
+            $todo = ( $fileinfo->isDir() ? 'rmdir' : 'unlink' );
+            $todo( $fileinfo->getRealPath() );
+        }
+    }
+
+    /**
+     * @param $data
+     * @param $action
+     * @return string
+     */
+    private function buildLogEntry($data, $action) {
+        $logText =  '######################################################################' . "\n" .
+            'Date & Time: ' . date("Y/m/d H:i:s", time()) . "\n" .
+            'Action: ' . $action . "\n" .
+            'Data:' . "\n" .
+            '----------------------------------------------------------------------' . "\n" .
+            $data . "\n" .
+            '----------------------------------------------------------------------' . "\n";
+        return $logText;
+    }
+
+    /**
+     * @param $data
+     */
+    private function writeLog($data): void
+    {
+        file_put_contents('logs/webhook_log_'.date("j.n.Y-H").'.log', $data, FILE_APPEND);
     }
 }
